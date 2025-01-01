54534a539659170bc266eb6fcd2ba23ea87baf25
Ayush Kapoor <ak5303858@gmail.com>
2025-01-01 15:33:17 +0530

    Update script.js
    added form working

--- portfolio/script.js
+++ portfolio/script.js
@@ -63,3 +63,43 @@ const typed = new Typed('.multiple-text', {
 document.getElementById('phone').addEventListener('input', function (e) {
    e.target.value = e.target.value.replace(/[^\d]/g, '');
 });
+
+
+const form = document.querySelector("form");
+const fname=document.getElementById("name");
+const femail=document.getElementById("email");
+const fphone=document.getElementById("phone");
+const fsubject=document.getElementById("subject");
+const fmessage=document.getElementById("message");
+function sendEmail()
+{ 
+   const b_message = `Name: ${fname.value}<br>Email: ${femail.value}<br>Phone: ${fphone.value}<br>Message: ${fmessage.value}`;
+
+   Email.send({
+      Host : "smtp.elasticemail.com",
+      Username : "ak5303858@gmail.com",
+      Password : "CCB72BF9DD2A3FD46AA8EE62A8354F4AE8E5",
+      To : 'ak5303858@gmail.com',
+      From : "ak5303858@gmail.com",
+      Subject : fsubject.value,
+      Body : b_message
+  }).then(
+    message => {
+      if(message=="OK")
+      {
+         Swal.fire({
+            title: "Success!",
+            text: "Message Sent Successfully!",
+            icon: "success"
+          });
+      }
+    }
+  );
+}
+
+form.addEventListener("submit",(e)=>
+{
+   e.preventDefault();
+   sendEmail();
+   document.querySelector("form").reset();
+})
