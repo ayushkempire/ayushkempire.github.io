cabc5aa2c7157dcfcd0ff033e84ce192019ce247
Ayush Kapoor <ak5303858@gmail.com>
2025-01-04 18:52:08 +0530

    Update script.js

--- portfolio/script.js
+++ portfolio/script.js
@@ -32,26 +32,6 @@ window.onscroll = () => {
    navbar.classList.remove('active');
 };
 
-ScrollReveal({
-   // reset: true,
-   distance: '80px',
-   duration: 2000,
-   delay: 200
-});
-
-ScrollReveal().reveal('.home-content, .heading', {
-   origin: 'top'
-});
-ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', {
-   origin: 'bottom'
-});
-ScrollReveal().reveal('.home-content h1, .about-img', {
-   origin: 'left'
-});
-ScrollReveal().reveal('.home-content p, .about-content', {
-   origin: 'right'
-});
-
 const typed = new Typed('.multiple-text', {
    strings: ['Frontend Developer', 'Graphic Designer', 'Editor'],
    typeSpeed: 100,
